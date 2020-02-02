2c0552de2bd6c0ad43b021c9284c94c19e63d5b9
onellvine <50703714+onellvine@users.noreply.github.com>
2020-02-02 12:22:52 +0300

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+
+<!-- core values -->
+<div class="row mt-5">
+    <div class="container text-center">
+        <h2>Our Values</h2>
+        <p class="description info-color">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+        </p>
+
+    </div>
+</div>
+
+<!-- mission & vision -->
+<div class="container mt-5 row">
+<div class="col-md-6 mr-3 mb-3"">
+    
+	<img src="images/mission.png" class="mr-3 mb-3"> Vision
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+    </p>
+    </div>
+    <div class='col-md-5'>
+    <img src="images/mission.png" class="mr-3 mb-3">Mission
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+        </p>
+    </div>
+
+    </div>
+
+</html>
